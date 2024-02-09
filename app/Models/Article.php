fd24812cd98b9c7f402e2c6aa8cc3b1ca59329e4
Adexe Reyes <adexe.reyes.cb@gmail.com>
2024-02-09 14:56:17 +0000

    relations Category-Article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
@@ -14,4 +15,9 @@ class Article extends Model
         'price',
         'stock',
     ];
+
+    public function category(): BelongsTo
+    {
+        return  $this->belongsTo(Category::class);
+    }
 }
